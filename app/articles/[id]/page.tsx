9902ce148a210a17a1709f1f1550982fac81c583
dileep <cdileepkumar22@gmail.com>
2025-03-04 01:00:46 +0530

    changesd to promise

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -2,36 +2,39 @@ import ArticleDetailPage from '@/components/articles/article-detail-page'
 import { prisma } from '@/lib/prisma'
 import React from 'react'
 
-type ArticleDetailPage={
-  params: {
-    id: string
+type ArticleDetailPageProps = {
+  params: Promise<{ id: string }>
 }
-}
-const page:React.FC<ArticleDetailPage> = async({params}) => {
-  const { id } = params
-  const article =await prisma.articles.findUnique({
-    where:{
+
+const page: React.FC<ArticleDetailPageProps> = async ({ params }) => {
+  const { id } = await params // Handle params as a Promise
+
+  if (!id) {
+    return <h1 className="text-center text-red-500">Invalid Article ID</h1>
+  }
+
+  const article = await prisma.articles.findUnique({
+    where: {
       id
-    },include:{
-      author:{
-        select:{
-          name:true,
-          email:true,
-          ImageUrl:true
+    },
+    include: {
+      author: {
+        select: {
+          name: true,
+          email: true,
+          ImageUrl: true
         }
       }
     }
   })
 
- 
-
-  if(!article){
-    return <h1> Article Not Found</h1>
+  if (!article) {
+    return <h1 className="text-center text-red-500">Article Not Found</h1>
   }
 
   return (
-    <div>
-     <ArticleDetailPage article={article}/>
+    <div className="container mx-auto p-6">
+      <ArticleDetailPage article={article} />
     </div>
   )
 }
